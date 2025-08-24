010ad94bdd9df3287991ba60d636d06e1b294133
Juniel Gavilan Sanchez <junielgavilan973@gmail.com>
2025-08-24 15:56:29 -0400

    Add files via upload

---
+++ horse_colic/import_data.py
@@ -0,0 +1,8 @@
+cln = ['surgery', 'age', 'hospital_number', 'rectal_temp', 'pulse', 'respiratory_rate', 'temp_of_extremities',
+       'peripheral_pulse', 'mucous_membrane', 'capillary_refill_time', 'pain', 'peristalsis', 'abdominal_distention',
+       'nasogastric_tube', 'nasogastric_reflux', 'nasogastric_reflux_ph', 'rectal_exam_feces', 'abdomen', 
+       'packed_cell_volume', 'total_protein', 'abdomo_appearance', 'abdomo_protein', 'outcome', 'surgical_lesion', 
+       'lesion_1', 'lesion_2', 'lesion_3', 'cp_data']
+
+df = pd.read_csv('horse-colic.csv', names=cln, engine='pyarrow')
+df.head()
